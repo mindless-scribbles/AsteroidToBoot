0d334ddcb72396716736f3be6e2e8e433c59a23b
dondecastro <dondecastro@gmail.com>
2026-03-06 12:10:42 -0800

    creat constants and logger files. First pass at a screen in pygame

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
 
 
 def main():
     version = pygame.version.ver
+    print(version)
     print("Starting Asteroids with pygame version: 2.6.1")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH} ")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
