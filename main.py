09f2ce0b8fc761fbc458b7dc683d57f496dc24b6
dondecastro <dondecastro@gmail.com>
2026-03-06 15:35:44 -0800

    create player and player control

--- main.py
+++ main.py
@@ -15,16 +15,22 @@ def main():
     clock = Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
-        dt = clock.tick(60) / 1000
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
